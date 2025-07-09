e2576de6050fbb7ba3f93ae10367fefbbc7c4bd7
Hoang VD <dunghv1@bsscommerce.com>
2025-07-09 13:56:17 +0700

    Commit a mess

--- down.sh
+++
@@ -1,2 +0,0 @@
-docker compose -f docker-compose.blue.yml -p blue_env down;
-docker compose -f docker-compose.green.yml -p green_env down;
--- stop.sh
+++
@@ -1,2 +0,0 @@
-docker compose -f docker-compose.blue.yml -p blue_env stop;
-docker compose -f docker-compose.green.yml -p green_env stop;
